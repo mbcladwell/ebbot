144a0b3921a2c7e3c7d542a0602fc698bf374f5f
mbcladwell <mbcladwell@gmail.com>
2024-09-20 06:53:09 -0400

    autocommit

--- scripts/tweet.sh
+++ scripts/tweet.sh
@@ -2,5 +2,5 @@
 export LC_ALL="C"
 export GUILE_LOAD_PATH=guileloadpath
 export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
-guileexecutable -L . -e '(babweb lib twitter)' -s $HOMEbabwebstorepath/share/guile/site/3.0/babweb/lib/twitter.scm
+guileexecutable -L . -e '(ebbot twitter)' -s $HOMEebbotstorepath/share/guile/site/3.0/ebbot/lib/twitter.scm
 
