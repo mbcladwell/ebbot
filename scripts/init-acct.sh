0e92a934796e86092414517fc2bc395cb4f478f6
mbcladwell <mbcladwell@gmail.com>
2024-09-20 08:20:08 -0400

    autocommit

--- scripts/init-acct.sh
+++ scripts/init-acct.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-
-export LC_ALL="C"
 export GUILE_LOAD_PATH=guileloadpath
 export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
 guileexecutable -e '(ebbot initacct)' -s ebbotstorepath/share/guile/site/3.0/ebbot/initacct.scm $1 $2 $3
