bbc8d311e80a876b983c2cc834f0b56a3d7a6d68
mbcladwell <mbcladwell@gmail.com>
2024-09-30 07:14:54 -0400

    autocommit

--- scripts/masttoot.sh
+++ scripts/masttoot.sh
@@ -2,5 +2,5 @@
 export LC_ALL="C"
 export GUILE_LOAD_PATH=guileloadpath
 export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
-guileexecutable -L . -e '(ebbot mastodon)' -s $HOMEebbotstorepath/share/guile/site/3.0/ebbot/mastodon.scm
+guileexecutable -L . -e '(ebbot mastodon)' -s $HOMEebbotstorepath/share/guile/site/3.0/ebbot/mastodon.scm $1
 
